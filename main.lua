e535bb5097a7dd29368666446b16065015aa329e
Benjamin Poon <benjamintpoon@gmail.com>
2018-02-18 22:14:26 -0600

    vscode build system, logging func on windows, file loader

--- main.lua
+++ main.lua
@@ -1,15 +1,40 @@
+dbg = true
 Object = require 'lib/classic/classic'
 require 'obj/Test'
 
 function love.load()
-  test_instance = Test()
-  image = love.graphics.newImage('assets/dora.png')
+  log('--Loading------')
+  local object_files = {}
+  recursiveEnumerate('obj', object_files)
+  requireFiles(object_files)
+end
+
+function recursiveEnumerate(folder, file_list)
+  local items = love.filesystem.getDirectoryItems(folder)
+  for _, item in ipairs(items) do
+    local file = folder .. '/' .. item
+    if love.filesystem.isFile(file) then
+      log('loading file: ', file)
+      table.insert(file_list, file)
+    elseif love.filesystem.isDirectory(file) then
+      recursiveEnumerate(file, file_list)
+    end
+  end
+end
+
+function requireFiles(files)
+  for _, file in ipairs(files) do
+    local file = file:sub(1, -5)
+    require(file)
+  end
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-  -- love.graphics.draw(image, love.math.random(0, 800), love.math.random(0, 600))
-  love.graphics.draw(image, 0, 0)
+end
+
+function log(...)
+  if dbg then print(...) end
 end
